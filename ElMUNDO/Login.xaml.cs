1bd3d0ee36c6f50a4e7a6eb23e4765be0e104792
Jasson <Jasson@DESKTOP-S5KM2ME>
2024-09-29 12:44:49 -0600

    Diseño modificado de login, Diseño de logo agregado, Fuentes Agregadas para el diseño

--- ElMUNDO/Login.xaml.cs
+++ ElMUNDO/Login.xaml.cs
@@ -2,23 +2,34 @@ namespace ElMUNDO;
 
 public partial class Login : ContentPage
 {
-	public Login()
+    private readonly SQLiteLogin.DatabaseHelper _databaseHelper;
+    public Login()
 	{
 		InitializeComponent();
-	}
+        string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "users.db");
+        _databaseHelper = new SQLiteLogin.DatabaseHelper(dbPath);
+
+    }
 
     private async void loginBTN_Clicked(object sender, EventArgs e)
     {
         string username = userentry.Text;
         string password = passwordentry.Text;
-        if (username == "Bryan" && password == "123")
+
+        var user = await _databaseHelper.GetUserAsync(username);
+        if (user != null && user.Password == password)
         {
-            await DisplayAlert("Inicio de sesión", "¡Inicio de sesión exitoso!", "OK");
+            await DisplayAlert("Success", "Login successful!", "OK");
             await Navigation.PushAsync(new Pages.TabbedPageE());
         }
         else
         {
-            await DisplayAlert("Inicio de sesión fallido", "Usuario o contraseña incorrectos.", "OK");
+            await DisplayAlert("Error", "Invalid username or password", "OK");
         }
+    }
+
+    private  void registrarBTN_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new CrearCuenta());
     }
 }
